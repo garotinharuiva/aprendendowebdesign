b7678b5d54a1656fce85895e061f49934394b10f
Gisela <gisela.judith@gmail.com>
2015-05-10 20:34:25 -0300

    Primeiro commit

---
+++ Novo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Novo.Startup))]
+namespace Novo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
